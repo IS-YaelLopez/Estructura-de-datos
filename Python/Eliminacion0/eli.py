42240c2115a7aabe1dd8a6bfcd5133601b929366
IS-YaelLopez <yaellopezes10@gmail.com>
2026-09-03 10:25:22 -0700

    Primer commit

--- Python/Eliminacion0/eli.py
+++ Python/Eliminacion0/eli.py
@@ -3,8 +3,17 @@ inputArr = [11, 21, 31, 41, 51, 61]
 print("Antes de la eliminacion, el array es: ")
 for j in range(len(inputArr)):
     print(inputArr[j], end= " ")
+
 #Eliminado el primer elemento del inputArr[0]
 print("\nDespues de la eliminacion, el array es: ")
 inputArr.pop(0)
+for j in range(len(inputArr)):
+    print(inputArr[j], end=" ")
+
+
+#Eliminado el ultimo elemento del inputArr[-1]
+inputArr = [11, 21, 31, 41, 51, 61]
+print("\nDespues de la eliminacion, el array es: ")
+inputArr.pop(-1)
 for j in range(len(inputArr)):
     print(inputArr[j], end=" ")
